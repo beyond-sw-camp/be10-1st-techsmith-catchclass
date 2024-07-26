9a4b966f01321ce97e4afedf9e87971e447a3644
hyomee2 <s5763305@naver.com>
2024-07-26 17:14:18 +0900

    feat: #13 비밀번호 찾기
    로그인 아이디와 이름, 전화번호로 비밀번호를 찾는 기능 구현했습니다.

---
+++ catch-class/users/findPwd.sql
@@ -0,0 +1,16 @@
+-- 비밀번호 찾기
+DROP PROCEDURE IF EXISTS FindPwd;
+DELIMITER //
+
+CREATE PROCEDURE FindPwd(IN login_id_para VARCHAR(20), IN name_para VARCHAR(15), IN telphone_para DATE)
+BEGIN
+	SELECT pwd
+		FROM users
+	WHERE login_id=login_id_para 
+		AND user_name=name_para 
+		AND telphone=telphone_para;
+END //
+
+DELIMITER ;
+
+-- CALL FindPwd('user2', 'Bob', '010-2345-6789');
