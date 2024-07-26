5eefce35916814f3798154d6753c16c38996b891
hyomee2 <s5763305@naver.com>
2024-07-26 17:45:49 +0900

    fix: #13 비밀번호 찾기 기능 수정
    띄어쓰기 및 데이터 타입 수정했습니다.

--- catch-class/users/findPwd.sql
+++ catch-class/users/findPwd.sql
@@ -2,13 +2,13 @@
 DROP PROCEDURE IF EXISTS FindPwd;
 DELIMITER //
 
-CREATE PROCEDURE FindPwd(IN login_id_para VARCHAR(20), IN name_para VARCHAR(15), IN telphone_para DATE)
+CREATE PROCEDURE FindPwd(IN login_id_para VARCHAR(20), IN name_para VARCHAR(15), IN telphone_para VARCHAR(15))
 BEGIN
 	SELECT pwd
 		FROM users
-	WHERE login_id=login_id_para 
-		AND user_name=name_para 
-		AND telphone=telphone_para;
+	WHERE login_id = login_id_para 
+		AND user_name = name_para 
+		AND telphone = telphone_para;
 END //
 
 DELIMITER ;
