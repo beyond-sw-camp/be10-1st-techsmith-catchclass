412f609e608e10fcffc74cba8e8690d95be444d3
whoogeek <rlarjswn0303@naver.com>
2024-07-26 18:56:25 +0900

    feat: 후기 조회 별점순 기능 추가

---
+++ catch-class/review/viewReviewsRating.sql
@@ -0,0 +1,22 @@
+-- 후기 목록 별점 조회 프로시저
+
+DELIMITER //
+
+CREATE PROCEDURE ViewReviewsRating()
+BEGIN
+		SELECT
+		       b.re_title
+		     , b.re_content
+		     , b.rating
+		     , b.re_time
+		     , COALESCE(c.user_name, '알수없음')
+		  FROM review b
+		  JOIN reservation a ON a.reservation_id = b.reservation_id
+		  left JOIN users c ON c.user_id = a.user_id
+		WHERE re_status = TRUE
+		ORDER BY rating DESC;
+END //
+
+DELIMITER ;
+
+CALL ViewReviewsRating();
