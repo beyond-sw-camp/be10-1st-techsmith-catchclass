0aad8cad7a5af227c86b5393274e06c584168550
KimJaehoon64 <0192985468@naver.com>
2024-07-28 00:12:19 +0900

    feat:#69 클래스 문의 등록

---
+++ catch-class/class/inquiryClass.sql
@@ -0,0 +1,39 @@
+-- #69. 원데이 클래스 문의 작성
+
+
+DROP PROCEDURE IF EXISTS InquiryClass;
+DELIMITER //
+
+CREATE PROCEDURE InquiryClass(
+    IN inq_title VARCHAR(255),       
+    IN inq_content TEXT,           
+    IN inq_secret TINYINT,            
+    IN user_id INT,                
+    IN class_id INT             
+)
+BEGIN
+    INSERT INTO inquiry (
+        inq_title,
+        inq_content,
+        inq_time,
+        inq_secret,
+        user_id,
+        class_id,
+        res_content,
+        res_time
+    )
+    VALUES (
+        inq_title,       
+        inq_content,     
+        LOCALTIMESTAMP(),
+        inq_secret,     
+        user_id,         
+        class_id,        
+        NULL,           
+        NULL           
+    );
+END //
+
+DELIMITER ;
+CALL InquiryClass('문의1', '문의내용1', 1, 3, 1);
+SELECT * FROM inquiry;	
