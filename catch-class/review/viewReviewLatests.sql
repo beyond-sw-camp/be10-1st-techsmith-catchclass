370f2f32b2a4af7afa74c9022ffe82caf923be03
whoogeek <rlarjswn0303@naver.com>
2024-07-27 22:22:17 +0900

    feat: #42 후기 목록 최신순 조회 기능 별칭 변경

--- catch-class/review/viewReviewLatests.sql
+++ catch-class/review/viewReviewLatests.sql
@@ -3,11 +3,11 @@ DELIMITER //
 CREATE PROCEDURE ViewReviewsTime()
 BEGIN
 		SELECT
-		       b.re_title
-		     , b.re_content
-		     , b.rating
-		     , b.re_time
-		     , COALESCE(c.user_name, '알수없음') -- 회원 정보 없는 경우 회원 이름 '알수없음'으로 변경
+		       b.re_title 후기제목
+		     , b.re_content 후기내용
+		     , b.rating 별점
+		     , b.re_time 후기작성시간
+		     , COALESCE(c.user_name, '알수없음') 회원명 -- 회원 정보 없는 경우 회원 이름 '알수없음'으로 변경
 		  FROM review b
 		  JOIN reservation a ON a.reservation_id = b.reservation_id
 		  left JOIN users c ON c.user_id = a.user_id
