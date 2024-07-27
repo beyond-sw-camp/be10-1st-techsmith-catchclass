4647159a6ae009c19656710a5d56495fd481544b
whoogeek <rlarjswn0303@naver.com>
2024-07-27 23:10:38 +0900

    feat: #44 후기 오래된 순으로 조회 기능 수정

--- catch-class/review/viewReviewOld.sql
+++ catch-class/review/viewReviewOld.sql
@@ -2,20 +2,25 @@ DELIMITER //
 
 CREATE PROCEDURE ViewReviewTimeR()
 BEGIN
-		SELECT
+		SELECT 
 		       b.re_title 후기제목
 		     , b.re_content 후기내용
 		     , b.rating 별점
 		     , b.re_time 후기작성시간
-		     , COALESCE(c.user_name, '알수없음') 회원명 -- 회원 탈퇴 시 회원 이름 '알수없음'으로 변경
+		     , COALESCE(c.user_name, '알수없음') 후기작성자
+		     , e.class_name 클래스이름
+		     , d.class_date 클래스날짜
+		     , d.start_time 클래스시간
 		  FROM review b
 		  JOIN reservation a ON a.reservation_id = b.reservation_id
 		  left JOIN users c ON c.user_id = a.user_id
+		  JOIN subclass d ON d.subclass_id = a.round_id
+		  JOIN class e ON e.class_id = d.class_id
 		WHERE re_status = TRUE
-		ORDER BY re_time; -- 오래된 순으로 정렬
+		ORDER BY re_time;
 END //
 
-DELIMITER ;	
+DELIMITER ;
 
 
 CALL ViewReviewTimeR();    
