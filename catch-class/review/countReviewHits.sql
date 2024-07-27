8dc4180909853e761a65cc9c08b9f1bef638e1cc
whoogeek <rlarjswn0303@naver.com>
2024-07-27 21:00:59 +0900

    feat: 좋아요 수 조회 기능 구현

---
+++ catch-class/review/countReviewHits.sql
@@ -0,0 +1,14 @@
+DELIMITER //
+
+CREATE PROCEDURE ViewLikesCountByReid(
+    IN input_re_id INT
+)
+BEGIN
+    SELECT COUNT(DISTINCT user_id) AS likes_count -- 등록된 유저 아이디 카운트
+    FROM review_hits
+    WHERE re_id = input_re_id; 
+END //
+
+DELIMITER ;
+
+CALL ViewLikesCountByReid(10);
