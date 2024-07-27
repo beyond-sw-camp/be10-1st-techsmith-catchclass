66c8e089240d4ae2913ddf0464ce606d7a2ab7b6
hyomee2 <s5763305@naver.com>
2024-07-27 18:28:24 +0900

    fix: #87 사업자 인증 신청 내역 조회 수정
    사업자 인증 신청 내역 전체 조회, cert_check 상태 별로 조회 모두 가능하게
    수정했습니다.

--- catch-class/admin/checkCert.sql
+++ catch-class/admin/checkCert.sql
@@ -1,15 +1,31 @@
--- 사업자 인증 신청 내역(미확인된 것) 조회
-DROP PROCEDURE IF EXISTS CheckCert;
+-- 사업자 인증 신청 내역 전체 조회
+DROP PROCEDURE IF EXISTS SelectCertAll;
 DELIMITER //
 
 CREATE PROCEDURE CheckCert()
 BEGIN
 	SELECT user_id, img_url
 	FROM image
-	WHERE img_ctg ='certification' 
-		AND cert_check ='unchecked';
+	WHERE img_ctg ='certification';
 END //
 
 DELIMITER ;
 
 -- CALL CheckCert();
+
+
+-- 사업자 인증 신청 내역 중 cert_check 상태 별로 조회
+DROP PROCEDURE IF EXISTS SelectCertByCertCheck;
+DELIMITER //
+
+CREATE PROCEDURE SelectCertByCertCheck(IN cert_check_para VARCHAR(20))
+BEGIN
+	SELECT user_id, img_url
+	FROM image
+	WHERE img_ctg ='certification' 
+		AND cert_check =cert_check_para;
+END //
+
+DELIMITER ;
+
+-- CALL SelectCertByCertCheck('reject');
