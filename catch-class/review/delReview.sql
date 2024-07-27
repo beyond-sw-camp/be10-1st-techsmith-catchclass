724757a6acc9f8a82d8cfc9e1de9e6c22f4b5259
whoogeek <rlarjswn0303@naver.com>
2024-07-27 20:13:03 +0900

    feat: 리뷰 제거 기능 추가

---
+++ catch-class/review/delReview.sql
@@ -0,0 +1,17 @@
+DELIMITER //
+
+CREATE PROCEDURE DeleteReview(
+    IN d_re_id bool
+)
+BEGIN
+   UPDATE review
+   SET re_status = FALSE,   -- 개시 상태 TRUE -> FALSE
+       re_del_time = NOW()
+   WHERE re_id = d_re_id;
+END //
+
+DELIMITER ;
+
+DROP PROCEDURE deletereview;
+
+CALL DeleteReview(13);
