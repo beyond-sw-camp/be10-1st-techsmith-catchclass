be3ede84f1464419e42e92a9d613e83663ebbf91
we4sley <galaxy138742@gmail.com>
2024-07-28 06:39:45 +0900

    feat: #156 쿠폰 상태 복원 프로시저 구현

--- catch-class/reservation/cancelReservation.sql
+++ catch-class/reservation/cancelReservation.sql
@@ -33,3 +33,36 @@ BEGIN
 END //
 
 DELIMITER ;
+
+-- 쿠폰 상태 복원 프로시저
+DELIMITER //
+
+CREATE PROCEDURE RestoreCouponStatus(
+    IN coupon_owner_id_param INT
+)
+BEGIN
+    DECLARE coupon_id_value INT;
+    
+    IF coupon_owner_id_param IS NOT NULL THEN
+        SELECT coupon_id
+        INTO coupon_id_value
+        FROM coupon_owner
+        WHERE coupon_owner_id = coupon_owner_id_param;
+
+        IF coupon_id_value IS NOT NULL THEN
+            UPDATE coupon
+            SET coupon_amount = coupon_amount + 1
+            WHERE coupon_id = coupon_id_value;
+
+            UPDATE coupon_owner
+            SET coupon_status = TRUE
+            WHERE coupon_owner_id = coupon_owner_id_param;
+        ELSE
+            SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '유효한 쿠폰이 없습니다.';
+        END IF;
+    ELSE
+        SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '유효한 쿠폰 소유자가 아닙니다.';
+    END IF;
+END //
+
+DELIMITER ;
