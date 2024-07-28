0a40683454abea4381b5db24eef4aac05a702a6b
KimJaehoon64 <0192985468@naver.com>
2024-07-28 18:25:54 +0900

    comment: 주석처리

--- catch-class/class/updateClassStatus.sql
+++ catch-class/class/updateClassStatus.sql
@@ -19,10 +19,10 @@ CALL UpdateClassStatus();
 SELECT * FROM subclass;
 
 -- 조건에 따른 테스트 구문 
-INSERT INTO subclass (tutor_name, subclass_content, reservation_limit, reservation_count, room_info, class_date, start_time, end_time, class_id) VALUES
-('종료test', '1', 20, 10, 'A1', '2024-08-01 10:00:00', '2024-07-20 10:00:00', '2024-07-20 12:00:00', 1),
-('모집중tes', '2', 20, 10, 'A2', '2024-08-01 10:00:00', '2024-08-01 10:00:00', '2024-08-01 12:00:00', 1),
-('모집완료test', '3', 20, 20, 'A3', '2024-08-01 10:00:00', '2024-08-01 10:00:00', '2024-08-01 12:00:00', 1)
+-- INSERT INTO subclass (tutor_name, subclass_content, reservation_limit, reservation_count, room_info, class_date, start_time, end_time, class_id) VALUES
+-- ('종료test', '1', 20, 10, 'A1', '2024-08-01 10:00:00', '2024-07-20 10:00:00', '2024-07-20 12:00:00', 1),
+-- ('모집중tes', '2', 20, 10, 'A2', '2024-08-01 10:00:00', '2024-08-01 10:00:00', '2024-08-01 12:00:00', 1),
+-- ('모집완료test', '3', 20, 20, 'A3', '2024-08-01 10:00:00', '2024-08-01 10:00:00', '2024-08-01 12:00:00', 1)
 
 -- CALL UpdateClassStatus();
 -- SELECT * FROM subclass;
