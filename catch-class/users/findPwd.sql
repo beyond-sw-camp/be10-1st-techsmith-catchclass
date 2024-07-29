fa328846149396fdee4f6f014f254e1ccc85978e
hyomee2 <s5763305@naver.com>
2024-07-29 09:24:08 +0900

    fix: #13 비밀번호 찾기 기능 수정
    회원 정보가 없을 때 예외처리 코드 추가했습니다.

--- catch-class/users/findPwd.sql
+++ catch-class/users/findPwd.sql
@@ -4,13 +4,25 @@ DELIMITER //
 
 CREATE PROCEDURE FindPwd(IN login_id_para VARCHAR(20), IN name_para VARCHAR(15), IN telphone_para VARCHAR(15))
 BEGIN
-	SELECT pwd
-		FROM users
-	WHERE login_id = login_id_para 
+	DECLARE result_count INT;
+	
+	SELECT COUNT(*) INTO result_count
+	FROM users
+	WHERE login_id = login_id_para
 		AND user_name = name_para 
 		AND telphone = telphone_para;
+	
+	IF result_count = 0 THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '일치하는 정보가 없습니다.';
+	ELSE
+		SELECT pwd
+			FROM users
+		WHERE login_id = login_id_para
+		AND user_name = name_para 
+		AND telphone = telphone_para;
+	END IF;
 END //
 
 DELIMITER ;
 
--- CALL FindPwd('user2', 'Bob', '010-2345-6789');
+CALL FindPwd('user2', 'Bob', '010-2345-6780');
