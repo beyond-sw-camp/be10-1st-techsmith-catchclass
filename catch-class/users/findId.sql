5ad9edde932835fc4c313c9215a075733410283f
hyomee2 <s5763305@naver.com>
2024-07-29 09:22:17 +0900

    fix: #11 아이디 찾기 기능 수정
    아이디 찾기 기능에 예외 처리 주가했습니다.(등록된 정보가 없을 경우)

--- catch-class/users/findId.sql
+++ catch-class/users/findId.sql
@@ -4,12 +4,23 @@ DELIMITER //
 
 CREATE PROCEDURE FindId(IN name_para VARCHAR(15), IN telphone_para VARCHAR(15))
 BEGIN
-	SELECT login_id
-		FROM users
+	DECLARE result_count INT;
+	
+	SELECT COUNT(*) INTO result_count
+	FROM users
 	WHERE user_name = name_para 
 		AND telphone = telphone_para;
+	
+	IF result_count = 0 THEN
+		SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = '일치하는 정보가 없습니다.';
+	ELSE
+		SELECT login_id
+			FROM users
+		WHERE user_name = name_para 
+			AND telphone = telphone_para;
+	END IF;
 END //
 
 DELIMITER ;
 
--- CALL FindId('Alice', '010-1234-5678');
+CALL FindId('Alice', '010-1234-5679');
